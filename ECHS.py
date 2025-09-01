c34c9b2a17f930916bdf721756c7f8f75a3c711c
Arete-Health-Tech <swarnimseth@aretehealth.tech>
2025-09-01 14:11:39 +0530

    Commited code with automated claimid generation process 2

--- ECHS.py
+++ ECHS.py
@@ -505,7 +505,7 @@ async def update_ocr_result(
 def generate_claim_id():
     try:
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(headless=True)
             page = browser.new_page()
 
             # Fetch latest referral
