f6d5fbbc244a3605cd22a9138f6fb6067783331a
Jyotisulakhe <jyotisulakhe123@gmail.com>
2025-10-04 15:38:06 +0530

    headless true

--- ECHS.py
+++ ECHS.py
@@ -933,7 +933,7 @@ def generate_claim_id():
             raise Exception(f"No account found for polyclinic {center_code}")
 
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(headless=True)
             page = browser.new_page()
 
             # --- Login page ---
